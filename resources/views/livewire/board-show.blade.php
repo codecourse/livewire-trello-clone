83d93c21129d42274950a4e6577eeac8e86f816d
Alex Garrett-Smith <alex@codecourse.com>
2024-05-17 11:51:58 +0100

    Episode 04

--- resources/views/livewire/board-show.blade.php
+++ resources/views/livewire/board-show.blade.php
@@ -4,4 +4,14 @@
             {{ $board->title }}
         </h2>
     </x-slot>
+
+    <div class="w-full p-6 overflow-x-scroll">
+        <div class="flex w-max space-x-6 bg-green-500 h-[calc(theme('height.screen')-64px-73px-theme('padding.12'))]">
+            @foreach (range(1, 10) as $column)
+                <div class="bg-blue-500 w-[260px]">
+                    Column
+                </div>
+            @endforeach
+        </div>
+    </div>
 </div>
